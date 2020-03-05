0ddb14c44d10e35a8c9694483d3bf1a5b1988020
DE-ALMEIDA-GOMES Kevin <Kevin.DE-ALMEIDA-GOMES@cpnv.ch>
2020-03-05 16:48:56 +0100

    Affichage aide debut

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+/*
+ * Titre : Bataille Navale
+ * Auteur : Kevin Gomes
+ * Date : 05 mars 2020
+ * Description : Programme permettant de jouer à la bataille navale en solo contre nous même (tout le temps gagnant)
+ * Version : 0.1
+ */
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    int aide=0;
+
+    printf("Bienvenue a la bataille navale\n\n");
+
+    printf("Appuyez sur 1 si vous voulez l'aide de jeu\n");
+
+
+
+    printf("\n\nAide: Bienvenue sur le jeu de bataille navale.\n");
+    printf("Voici les regles du jeu :\n");
+
+
+
+
+
+
+
+
     return 0;
 }
