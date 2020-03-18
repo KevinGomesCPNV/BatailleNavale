527679b845232f28fdade265ae1b3c70ea0d7595
unknown <kevin.de-almeida-gomes@cpnv.ch>
2020-03-18 22:46:33 +0100

    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# BatailleNavale
+BATAILLE NAVALE by Kevin Gomes (SI-C1b CPNV)
+
+Description:
+
+Le projet Bataille Navale est un jeu en cmd (Jouable contre nous même avec une seule map)
+
+
+Vous aurez besoin de : 
+
+● Un compilateur C (C est le language de programation, je vous conseille CLion) 
+						
+● Un système d'exploitation Windows
+
+Marche à suivre pour jouer :
+
+1. Installer git
+2. Faire un git bash here dans un dossier (clique droit à l'intérieur du dossier)
+3. Faire la commande Git clone https://github.com/KevinGomesCPNV/BatailleNavale.git puis Entrée
+4. Aller dans le dossier cmake-build-debug  et lancer  BatailleNavale.exe
+
+Voilà
+
+Bon jeu
+
+
--- main.c
+++ main.c
@@ -1,98 +1,205 @@
 /*
  * Titre : Bataille Navale
  * Auteur : Kevin Gomes
- * Date : 06 mars 2020
- * Description : Programme permettant de jouer à la bataille navale en solo contre nous même (tout le temps gagnant)
+ * Date : 18 mars 2020
+ * Description : Programme effectué dans le cadre du MA-20 et ICT-114 du CPNV permettant de jouer à la bataille navale en solo avec une grille de base
  * Version : 0.1
  */
 
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
+
+//Renvoie vers la fonction du menu de base
+void menuDeBase();
+
+                                        //==== MENU DE JEU====
+//Fonction du menu de jeux
+void jeux(){
+
+    //commande permettant de vider la page
+    system("cls");
+
+    char *val = "X";
+
+    int ligneHor;
+    int ligneVer;
+
+    int carte[10][10] = {
+            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 1, 1, 1, 1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    };
+
+    // affichage de la carte
+    printf("\nVoici la carte\n");
+    printf("Vous devez abattre tous les bateaux\n");
+    printf("Bonne chance\n\n");
+    // affichage des nombres du tableau horizontalement
+    printf("  1   2   3   4   5   6   7   8   9   10   \n");
+    for (int i = 0; i < 41; ++i)
+    {
+        printf("═");
+    }
+
+    printf("\n");
+    for (int ligne = 0; ligne < 10; ++ligne)
+    {
+        for (int colonne = 0; colonne < 10; ++colonne)
+        {
+            if (carte[ligne][colonne] == 1)
+            {
+                printf("║ %c ", val);
+            } else {
+                printf("║   ");
+            }
+        }
+        printf("║\n");
+        for (int i = 0; i < 41; ++i)
+        {
+            printf("═");
+        }
+        printf("\n");
+    }
+
+    //printf("Indiquez une ligne horizontale");
+    //scanf("%d",&ligneHor);
+    //printf("Indiquez une ligne verticale");
+    //scanf("%d",&ligeVer);
+
+
+    //Affiche le message "Appuyer sur une touche pour continuer"
+    system("pause");
+
+    //Retourne dans le menu de base
+    menuDeBase();
+
+
+}
+
+                                        //==== MENU D'AIDE ====
+//Menu de l'aide
+void menuAide(){
+
+    //commande permettant de vider la page
+    system("cls");
+
+    //Affichage de l'aide
+    printf("Aide:\n");
+    printf("\nVoici les regles du jeu :\n"
+           "Le but : Couler tous les bateaux adverses placés quelque part en mer (sur le plateau de jeu de 100 cases), avec des missiles.\n\n"
+           "Il y a 5 bateau:\n\n"
+           "1 porte-avion (5 cases)\n"
+           "1 croiseur (4 cases)\n"
+           "2 contre-torpilleurs (3 cases)\n"
+           "1 torpilleur (2 cases)\n\n");
+    printf("Vous devrez selectionner une ligne verticale puis celle horizontale\n\n");
+
+    //Affiche le message "Appuyer sur une touche pour continuer"
+    system("pause");
+
+    //Retourne dans le menu de base
+    menuDeBase();
+}
+
+                                        //==== MENU D'IDENTIFICATION ====
+//Fonction d'identification
+void identification(){
+
+    char identification[24];
+
+    //commande permettant de vider la page
+    system("cls");
+
+    //Enregistrer un identifiant (pour la version 1.0)
+    printf("Veuillez vous identifier: (uniquement le prénom, pas d'espace)\n");
+    scanf("%s", identification);
+
+    //Affiche le message "Appuyer sur une touche pour continuer"
+    system("\npause");
+
+    //Retourne dans le menu de base
+    menuDeBase();
+
+}
+                                        //==== MENU DE BASE ====
+//Fonction du menu de base
+void menuDeBase() {
+
+    //Commande permettant de vider la page
+    system("cls");
+
+    //Commande pour afficher les accents
+    SetConsoleOutputCP(65001);
+
+    //Variable mis à zero pour le choix du menu
+    int choix=0;
+
+    //Affichage du menu de base
+    printf("______         _          _  _  _         _   _                      _       \n"
+           "| ___ \\       | |        (_)| || |       | \\ | |                    | |      \n"
+           "| |_/ /  __ _ | |_  __ _  _ | || |  ___  |  \\| |  __ _ __   __ __ _ | |  ___ \n"
+           "| ___ \\ / _` || __|/ _` || || || | / _ \\ | . ` | / _` |\\ \\ / // _` || | / _ \\\n"
+           "| |_/ /| (_| || |_| (_| || || || ||  __/ | |\\  || (_| | \\ V /| (_| || ||  __/\n"
+           "\\____/  \\__,_| \\__|\\__,_||_||_||_| \\___| \\_| \\_/ \\__,_|  \\_/  \\__,_||_| \\___|\n"
+           "                                                                             \n"
+           "                                                                             ");
+    printf("\n"
+           "                 >_'--'__'\n"
+           "                _________!__________\n"
+           "               /   /   /   /   /   /\n"
+           "              /   /   /   /   /   /\n"
+           "             |   |   |   |   |   |\n"
+           "        __^  |   |   |   |   |   |\n"
+           "      _/@  \\  \\   \\   \\   \\   \\   \\\n"
+           "     S__   |   \\   \\   \\   \\   \\   \\         __\n"
+           "    (   |  |    \\___\\___\\___\\___\\___\\       /  \\\n"
+           "        |   \\             |                |  |\\|\n"
+           "        \\    \\____________!________________/  /\n"
+           "         \\ _______OOOOOOOOOOOOOOOOOOO________/\n"
+           "          \\________\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_______/\n\n\n");
+
+
+    printf("1 : Jouer\n");
+    printf("2 : Aide\n");
+    printf("3 : S'identifier\n");
+    printf("4 : Quitter\n\n");
+
+    //Choix vers les différents menu
+    printf("Choisissez une option : ");
+    scanf("%d",&choix);
+
+    //Switch permettant de renvoyer l'utilisateur dans les autres menu
+    switch(choix){
+        //Renvoie vers la fonction du menu de jeu
+        case 1:jeux() ;
+            break;
+        //Renvoie vers la fonction du menu d'aide
+        case 2:menuAide() ;
+            break;
+        //Renvoie vers la fonction d'identification
+        case 3:identification() ;
+            break;
+        //Commande qui fait quitter le programme
+        case 4: system("exit");
+
+    }
+}
+
+
 int main() {
 
+    //Commence le programme avec le menu de base qui permettra par la suite à accèder aux autres menu du programme
+    menuDeBase();
 
-
-    int choix;
-    char identification[24];
-    int recommencer=0;
-
-
-        do {
-            printf("______         _          _  _  _         _   _                      _       \n"
-                   "| ___ \\       | |        (_)| || |       | \\ | |                    | |      \n"
-                   "| |_/ /  __ _ | |_  __ _  _ | || |  ___  |  \\| |  __ _ __   __ __ _ | |  ___ \n"
-                   "| ___ \\ / _` || __|/ _` || || || | / _ \\ | . ` | / _` |\\ \\ / // _` || | / _ \\\n"
-                   "| |_/ /| (_| || |_| (_| || || || ||  __/ | |\\  || (_| | \\ V /| (_| || ||  __/\n"
-                   "\\____/  \\__,_| \\__|\\__,_||_||_||_| \\___| \\_| \\_/ \\__,_|  \\_/  \\__,_||_| \\___|\n"
-                   "                                                                             \n"
-                   "                                                                             ");
-            printf("\n"
-                   "                 >_'--'__'\n"
-                   "                _________!__________\n"
-                   "               /   /   /   /   /   /\n"
-                   "              /   /   /   /   /   /\n"
-                   "             |   |   |   |   |   |\n"
-                   "        __^  |   |   |   |   |   |\n"
-                   "      _/@  \\  \\   \\   \\   \\   \\   \\\n"
-                   "     S__   |   \\   \\   \\   \\   \\   \\         __\n"
-                   "    (   |  |    \\___\\___\\___\\___\\___\\       /  \\\n"
-                   "        |   \\             |                |  |\\|\n"
-                   "        \\    \\____________!________________/  /\n"
-                   "         \\ _______OOOOOOOOOOOOOOOOOOO________/\n"
-                   "          \\________\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_______/\n\n\n");
-                   //"%%%^^^^^%%%%%^^^^!!^%%^^^^%%%%%!!!!^^^^^^!%^^^%%%%!!^^\n"
-                   //"^^!!!!%%%%^^^^!!^^%%%%%^^!!!^^%%%%%!!!%%%%^^^!!^^%%%!!\n\n");
-
-            //code(s) pour des fonctionalitées spéciales
-            SetConsoleOutputCP(65001);  //pour les accents, etc...
-            //code(s) pour des fonctionalitées spéciales
-
-            printf("Bienvenue a la bataille navale\n\n");
-            printf("1. Jouer\n"
-                   "2. Aide\n"
-                   "3. Identification\n\n");
-
-            //work in progress identification
-            //printf(identification);
-
-            scanf("%d", &choix);
-
-
-            if (choix == 1) {
-                system("cls");
-                printf("\nWORK IN PROGRESS\n");
-            } else if (choix == 2) {
-                system("cls");
-                printf("Aide:\n");
-                printf("\nVoici les regles du jeu :\n"
-                       "Le but : Couler tous les bateaux adverses placés quelque part en mer (sur le plateau de jeu de 100 cases), avec des missiles.\n"
-                       "Il y a trois bateaux : de 1, 2, et 3 cases de long. Les bateaux ne touchent pas.\n"
-                       "Pour essayer de toucher les bateaux, vous devez entrez la coordonnée de la case où vous voulez lancer un missile (exemple: A8), puis tapez enter. Attention à bien rentrer la lettre en premier !\n"
-                       "\n"
-                       "Si le programme affiche « A l’eau », vous n’avez pas touché de bateau. Une croix s’affiche dans la case. \n"
-                       "\n"
-                       "Si le programme affiche « Touché », c’est que vous avez touché un bateau. La case se remplit d’un cercle.\n"
-                       "\n"
-                       "Si le programme affiche « Touché et coulé », ça signifie que le bateau entier à été touché et qu’il est donc coulé.\n"
-                       "Toutes les cases s'affichent avec un carré blanc. Vous devez ensuite cherchez les autres bateaux."
-                       "\n\n"
-                       "Si vous retirez sur une case que vous avez déjà visée, le programme vous avertira et vous laissera recommencer.\n"
-                       "\n"
-                       "Une fois que vous avez touché les 3 bateaux, la partie s'arrête et un message s’affiche vous annonçant que vous avez gagné.");
-            } else if (choix == 3) {
-                system("cls");
-                printf("Veuillez vous identifier:\n");
-                scanf("%s", identification);
-            }
-            printf("\n\n1. Retour au menu\n"
-                   "2. Quitter le programme\n\n");
-            scanf("%d", &recommencer);
-            if(recommencer==2)
-            {
-                exit(0);
-            }
-        }while(recommencer=1);
-
-    system("pause");
     return 0;
 }
+
